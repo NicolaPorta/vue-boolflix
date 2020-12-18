abec05ab64f736f7e5bec783ced88b722f2bac21
Nicola <nicola.porta00@gmail.com>
2020-12-18 13:42:16 +0100

    Ajax test

--- js/script.js
+++ js/script.js
@@ -2,10 +2,28 @@ var app = new Vue(
   {
     el: "#root",
     data: {
-
+      apiId: "12d47eadb7bade3dfdef75db545e889f",
+      searchInput: "",
+      films:[]
     },
     methods: {
-      
+      searchFilm: function() {
+        this.films=[];
+        if (this.searchInput !== "") {
+          axios.get("https://api.themoviedb.org/3/search/movie", {
+            params: {
+              api_key: this.apiId,
+              query: this.searchInput,
+              linguages: "it-IT"
+            }
+          }).then(
+            (response) => {
+              this.films = response.data.results;
+            }
+          );
+        }
+
+      }
     }
   }
 );
