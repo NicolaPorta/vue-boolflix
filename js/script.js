403ec1f9e25d2a252cc8fe6ee498200b430ffaa7
Nicola <nicola.porta00@gmail.com>
2020-12-21 11:27:25 +0100

    Milestone2 tv series + film + Milestone 3

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ var app = new Vue(
       apiId: "12d47eadb7bade3dfdef75db545e889f",
       searchInput: "",
       films:[],
-      sources:"",
-      change: 1,
-      star:""
+      series:[],
+      allVideos:[]
+      // data CHANGE PAGE
+      // sources:"",
+      // change: 1,
     },
     methods: {
-      searchFilm: function() {
+      searchVideo: function() {
         if (this.searchInput !== "") {
           this.ajaxCall();
         }
@@ -21,29 +23,46 @@ var app = new Vue(
             api_key: this.apiId,
             query: this.searchInput,
             language: "it-IT",
-            page: this.change
+            // page: this.change
           }
         }).then(
           (response) => {
-            this.sources = response.data;
-            this.films = response.data.results;
+            // this.sources = response.data;
+            this.films= response.data.results;
+          }
+        );
+        axios.get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: this.apiId,
+            query: this.searchInput,
+            language: "it-IT",
+            // page: this.change
+          }
+        }).then(
+          (response) => {
+            this.series = response.data.results;
+            this.allVideos = this.films.concat(this.series);
             this.roundVote();
+            this.allVideos.sort(function (a,b) {
+              return b.vote_average - a.vote_average;
+            });
           }
         );
       },
-      changePage: function() {
-        this.ajaxCall();
-      },
-      prevPage: function() {
-        this.change--;
-        this.ajaxCall();
-      },
-      nextPage: function() {
-        this.change++;
-        this.ajaxCall();
-      },
+      // CHANGE PAGE
+      // changePage: function() {
+      //   this.ajaxCall();
+      // },
+      // prevPage: function() {
+      //   this.change--;
+      //   this.ajaxCall();
+      // },
+      // nextPage: function() {
+      //   this.change++;
+      //   this.ajaxCall();
+      // },
       roundVote: function() {
-        this.films.forEach(
+        this.allVideos.forEach(
           (element) => {
             element.vote_average = Math.ceil(element.vote_average / 2);
           }
